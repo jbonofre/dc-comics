c1a5e3834394a0950231db4dd2f3340ef6c7aecb
JB Onofré <jb.onofre@dremio.com>
2023-05-12 15:48:28 +0200

    Add dc-comics-maven-plugin

---
+++ dc-comics-maven-plugin/src/main/java/com/dremio/cloud/comics/maven/GenerateCodeMojo.java
@@ -0,0 +1,10 @@
+package com.dremio.cloud.comics.maven;
+
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+
+@Mojo(name = "generate-code", defaultPhase = LifecyclePhase.PROCESS_RESOURCES, requiresDependencyResolution = ResolutionScope.COMPILE_PLUS_RUNTIME, threadSafe = true)
+public class GenerateCodeMojo extends io.quarkus.maven.GenerateCodeMojo {
+
+}
